c3ad3819613437bea94ae01ced225208854e6afb
leerobertdyer <leerobertdyer@gmail.com>
2024-07-29 00:19:53 -0400

    feat: started work on services page

--- src/app/(pages)/services/page.tsx
+++ src/app/(pages)/services/page.tsx
@@ -1,5 +1,14 @@
+import { Song } from "@/components/BeforeAndAfterPlayer/BeforeAndAfterPlayer";
+import { useState } from "react";
+
 export default function Services() {
+    const [trumpetSongs, setTrumpetSongs] = useState<Song[]>([]);
+
     return (
-        <h1>Services Page</h1>
+        <>
+        <h1>Trumpets</h1>
+        <h2>Mixing</h2>
+        <h3>Full Production</h3>
+        </>
     )
 }

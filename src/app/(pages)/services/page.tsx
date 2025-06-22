f4140487f7d3cbf825805240f6309b7b360fd3dc
RVleerobertdyer <ldyer@redventures.com>
2025-06-22 15:51:56 -0400

    linter

--- src/app/(pages)/services/page.tsx
+++ src/app/(pages)/services/page.tsx
@@ -1,9 +1,5 @@
-import { Song } from "@/components/BeforeAndAfterPlayer/BeforeAndAfterPlayer";
-import { useState } from "react";
 
 export default function Services() {
-    const [trumpetSongs, setTrumpetSongs] = useState<Song[]>([]);
-
     return (
         <>
         <h1>Trumpets</h1>
